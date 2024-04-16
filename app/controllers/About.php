87cce97ba18b8529af6425d4d942834027664cde
alhammanazil <107827210+Alhammanazil@users.noreply.github.com>
2024-04-16 07:29:51 +0700

    test

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,12 +1,21 @@
 <?php
 
-class About {
-    public function index()
+class About extends Controller{
+    public function index($nama = 'Alham', $pekerjaan = 'Programmer', $umur = 23)
     {
-        echo 'Halo, nama saya Alham';
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-        echo 'About/page' ;
+        $data['judul'] = 'Page';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
---
+++ app/views/templates/footer.php
@@ -0,0 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+<script src="http://localhost/php-mvc/public/js/bootstrap.min.js"></script>
+</body>
+</html>
